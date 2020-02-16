ecee906c9a62548cc411adfb620f19845fc72b5a
Jake Caban-Tomski <jacque006@gmail.com>
2020-02-16 01:27:45 -0700

    Demo showing token modification

--- contracts/mETHToken.sol
+++ contracts/mETHToken.sol
@@ -7,15 +7,20 @@ import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
 contract mETHToken is ERC20, ERC20Detailed, ERC20Mintable {
-    constructor()
+    constructor(uint256 initialSupply)
         ERC20Detailed("moonETH", "mETH", 18)
         public
     {
-
+        _mint(msg.sender, initialSupply);
     }
 }
 
 contract mETHCrowdsale is Crowdsale, MintedCrowdsale {
+    // Naive CDP/Vault simulation
+    uint256 public collateralAmount;
+
+    uint256 public tokenRatio;
+
     constructor(
         uint256 rate, // rate in TKNbits
         address payable wallet,
@@ -25,23 +30,39 @@ contract mETHCrowdsale is Crowdsale, MintedCrowdsale {
         Crowdsale(rate, wallet, token)
         public
     {
+        collateralAmount = token.totalSupply();
+        tokenRatio = 100;
+    }
 
+    // Simulate process change increasing amount of stored wei after rebalance.
+    function addCollateral(uint256 amount) public {
+        collateralAmount += amount;
+        tokenRatio = 100 * token().totalSupply() / collateralAmount;
     }
 
     // Override this method to have a way to add business logic to your crowdsale when buying
     // Returns weiAmount times rate by default
     function _getTokenAmount(uint256 weiAmount) internal view returns(uint256) {
-        return super._getTokenAmount(weiAmount);
+        return super._getTokenAmount(weiAmount) * tokenRatio / 100;
+    }
+
+    function _updatePurchasingState(address beneficiary, uint256 weiAmount) internal {
+        addCollateral(weiAmount);
+
+        return super._updatePurchasingState(beneficiary, weiAmount);
     }
 }
 
 contract mETHCrowdsaleDeployer {
+    ERC20Mintable public token;
+    Crowdsale public crowdsale;
+
     constructor()
         public
     {
-        ERC20Mintable token = new mETHToken();
+        token = new mETHToken(100);
 
-        Crowdsale crowdsale = new mETHCrowdsale(
+        crowdsale = new mETHCrowdsale(
             1,
             msg.sender, // send Ether to the deployer. TODO Send to CDP contract
             token
--- test/crowdsale.test.js
+++ test/crowdsale.test.js
@@ -1,20 +1,55 @@
 const mETHToken = artifacts.require("mETHToken");
 const mETHCrowdsale = artifacts.require("mETHCrowdsale");
-// const mETHCrowdsaleDeployer = artifacts.require("mETHCrowdsaleDeployer");
+const mETHCrowdsaleDeployer = artifacts.require("mETHCrowdsaleDeployer");
 
 contract('mETHTokensCrowdsale', ([owner, wallet, investor, otherInvestor]) => {    
     beforeEach(async function () {
-        this.token = await mETHToken.new(); 
-        this.crowdsale = await mETHCrowdsale.new(1, wallet, this.token);
+        this.deployer = await mETHCrowdsaleDeployer.deployed();
+        this.token = await mETHToken.at(await this.deployer.token());
+        this.crowdsale = await mETHCrowdsale.at(await this.deployer.crowdsale());
     });
     
-    // it("allows token purchases", async () => {
-    //     const investmentAmount = ether(1);
-    //     const expectedTokenAmount = RATE.mul(investmentAmount);
+    it("allows token purchases with less generated with larger pool", async function () {
+        const investmentWei = 100;
+        const growthWei = 100;
+
+        const getTotalSupply = async () => {
+            const totalSupplyRes = await this.token.totalSupply();
+            return totalSupplyRes.toNumber();
+        };
+
+        const getInvestorBalance = async (investor) => {
+            const investorBalanceRes = await this.token.balanceOf(investor);
+            return investorBalanceRes.toNumber();
+        };
+
+        const divider = '------------------------------------------------------';
+        console.log();
+        console.log(divider);
+        console.log();
+
+        const startingSupply = await getTotalSupply();
+        console.log(`Starting token supply (wei):`, startingSupply);
+        assert.equal(startingSupply, 100);
     
-    //     await this.crowdsale.buyTokens(investor, { value: investmentAmount, from: investor }).should.be.fulfilled;
+        console.log(`Investor sends 100 wei, get 100 tokens (wei)`);
+        await this.crowdsale.buyTokens(investor, { value: investmentWei, from: investor });
     
-    //     (await this.token.balanceOf(investor)).should.be.bignumber.equal(expectedTokenAmount);
-    //     (await this.token.totalSupply()).should.be.bignumber.equal(expectedTokenAmount);    
-    // });
+        assert.equal(await getInvestorBalance(investor), 100);
+        assert.equal(await getTotalSupply(), 200);
+
+
+        console.log(`ETH CDP Collateral grows by ${growthWei} wei`);
+        await this.crowdsale.addCollateral(growthWei);
+
+        console.log(`Another Investor sends 100 wei, get 66 tokens (wei) due to CDP collateral growth`);
+        await this.crowdsale.buyTokens(otherInvestor, { value: investmentWei, from: otherInvestor });
+    
+        assert.equal(await getInvestorBalance(otherInvestor), 66);
+        assert.equal(await getTotalSupply(), 266);
+
+        console.log();
+        console.log(divider);
+        console.log();
+    });
 });
